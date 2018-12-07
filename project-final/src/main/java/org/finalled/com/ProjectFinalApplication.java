98a5901bba2f1d5fde6e201732981e2080605270
user <melwinpinto.tri@gmail.com>
2018-12-07 14:19:52 +0530

    First Commit from eclipse

---
+++ project-final/src/main/java/org/finalled/com/ProjectFinalApplication.java
@@ -0,0 +1,12 @@
+package org.finalled.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFinalApplication.class, args);
+	}
+}
---
+++ project-final/src/test/java/org/finalled/com/ProjectFinalApplicationTests.java
@@ -0,0 +1,16 @@
+package org.finalled.com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectFinalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
