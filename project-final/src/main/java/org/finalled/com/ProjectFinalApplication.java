c225c0f59349ffdf53f192115ffdf5716f720f7e
user <melwinpinto.tri@gmail.com>
2019-01-23 18:12:08 +0530

    UploadFile URL method

--- project-final/src/main/java/org/finalled/com/ProjectFinalApplication.java
+++ project-final/src/main/java/org/finalled/com/ProjectFinalApplication.java
@@ -1,12 +1,16 @@
 package org.finalled.com;
 
+import org.finalled.com.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
- @ComponentScan(basePackages="org.finalled.com") 
-
+@ComponentScan(basePackages="org.finalled.com") 
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class ProjectFinalApplication {
 
 	public static void main(String[] args) {
